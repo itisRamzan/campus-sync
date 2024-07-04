9d08b1ee7aa69c4964dfb4b9f544ed4d0532017b
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-07-04 16:26:04 +0530

    FIX: Added DB connection before webhooks actions

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -5,6 +5,7 @@ import Faculty from "@/models/Faculty.Model";
 import Student from "@/models/Student.Model";
 import User from "@/models/User.Model";
 import College from "@/models/College.Model";
+import { connectDB } from "@/lib/database/connectDB";
 
 export async function POST(req: Request) {
     const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
@@ -44,112 +45,121 @@ export async function POST(req: Request) {
     }
 
     const eventType = evt.type;
-    if (eventType === "user.created") {
-        if (evt.data.organization_memberships) {
-            let role = evt.data.organization_memberships[0].role;
-            if (role === "org:faculty") {
-                await Faculty.create({
-                    clerkID: evt.data.id,
-                    college: evt.data.organization_memberships[0].organization.private_metadata?._id, // college mongoose ID
-                    firstName: evt.data.first_name,
-                    lastName: evt.data.last_name,
-                    email: evt.data.email_addresses[0].email_address,
-                    photo: evt.data.image_url,
-                });
+    try {
+        await connectDB();
+        if (eventType === "user.created") {
+            if (evt.data.organization_memberships) {
+                let role = evt.data.organization_memberships[0].role;
+                if (role === "org:faculty") {
+                    await Faculty.create({
+                        clerkID: evt.data.id,
+                        college: evt.data.organization_memberships[0].organization.private_metadata?._id, // college mongoose ID
+                        firstName: evt.data.first_name,
+                        lastName: evt.data.last_name,
+                        email: evt.data.email_addresses[0].email_address,
+                        photo: evt.data.image_url,
+                    });
+                }
+                if (role === "org:student") {
+                    await Student.create({
+                        clerkID: evt.data.id,
+                        college: evt.data.organization_memberships[0].organization.private_metadata?._id, // college mongoose ID
+                        firstName: evt.data.first_name,
+                        lastName: evt.data.last_name,
+                        email: evt.data.email_addresses[0].email_address,
+                        photo: evt.data.image_url,
+                        department: evt.data.private_metadata?.department,
+                        semester: evt.data.private_metadata?.semester,
+                        rollNo: evt.data.private_metadata?.rollNo,
+                    });
+                }
             }
-            if (role === "org:student") {
-                await Student.create({
-                    clerkID: evt.data.id,
-                    college: evt.data.organization_memberships[0].organization.private_metadata?._id, // college mongoose ID
-                    firstName: evt.data.first_name,
-                    lastName: evt.data.last_name,
-                    email: evt.data.email_addresses[0].email_address,
-                    photo: evt.data.image_url,
-                    department: evt.data.private_metadata?.department,
-                    semester: evt.data.private_metadata?.semester,
-                    rollNo: evt.data.private_metadata?.rollNo,
-                });
+            await User.create({
+                clerkID: evt.data.id,
+                firstName: evt.data.first_name,
+                lastName: evt.data.last_name,
+                email: evt.data.email_addresses[0].email_address,
+                photo: evt.data.image_url,
+                role: "user",
+            });
+        }
+        if (eventType === "user.updated") {
+            if (evt.data.organization_memberships) {
+                let role = evt.data.organization_memberships[0].role;
+                if (role === "org:faculty") {
+                    await Faculty.findOneAndUpdate({ clerkID: evt.data.id }, {
+                        firstName: evt.data.first_name,
+                        lastName: evt.data.last_name,
+                        email: evt.data.email_addresses[0].email_address,
+                        photo: evt.data.image_url,
+                    });
+                }
+                if (role === "org:student") {
+                    await Student.findOneAndUpdate({ clerkID: evt.data.id }, {
+                        firstName: evt.data.first_name,
+                        lastName: evt.data.last_name,
+                        email: evt.data.email_addresses[0].email_address,
+                        photo: evt.data.image_url,
+                        department: evt.data.private_metadata?.department,
+                        semester: evt.data.private_metadata?.semester,
+                        rollNo: evt.data.private_metadata?.rollNo,
+                    });
+                }
             }
         }
-        await User.create({
-            clerkID: evt.data.id,
-            firstName: evt.data.first_name,
-            lastName: evt.data.last_name,
-            email: evt.data.email_addresses[0].email_address,
-            photo: evt.data.image_url,
-            role: "user",
+        if (eventType === "user.deleted") {
+            await User.findOneAndDelete({ clerkID: evt.data.id });
+            await Faculty.findOneAndDelete({ clerkID: evt.data.id });
+            await Student.findOneAndDelete({ clerkID: evt.data.id });
+        }
+        if (eventType === "organization.created") {
+            await College.create({
+                admin: evt.data.private_metadata?._id, // admin mongoose ID
+                name: evt.data.name,
+                location: evt.data.private_metadata?.location,
+                logo: evt.data.image_url,
+            });
+        }
+        if (eventType === "organization.updated") {
+            await College.findOneAndUpdate({ clerkID: evt.data.id }, {
+                name: evt.data.name,
+                location: evt.data.private_metadata?.location,
+                logo: evt.data.image_url,
+            });
+        }
+        if (eventType === "organization.deleted") {
+            await College.findOneAndDelete({ clerkID: evt.data.id });
+        }
+        if (eventType === "organizationMembership.created") {
+            if (evt.data.role === "org:faculty") {
+
+            }
+            if (evt.data.role === "org:student") {
+
+            }
+        }
+        if (eventType === "organizationMembership.updated") {
+            if (evt.data.role === "org:faculty") {
+
+            }
+            if (evt.data.role === "org:student") {
+
+            }
+        }
+        if (eventType === "organizationMembership.deleted") {
+            if (evt.data.role === "org:faculty") {
+
+            }
+            if (evt.data.role === "org:student") {
+
+            }
+        }
+    }
+    catch (err: any) {
+        console.error("Error processing webhook:", err.message);
+        return new Response("Error occured", {
+            status: 400
         });
-    }
-    if (eventType === "user.updated") {
-        if (evt.data.organization_memberships) {
-            let role = evt.data.organization_memberships[0].role;
-            if (role === "org:faculty") {
-                await Faculty.findOneAndUpdate({ clerkID: evt.data.id }, {
-                    firstName: evt.data.first_name,
-                    lastName: evt.data.last_name,
-                    email: evt.data.email_addresses[0].email_address,
-                    photo: evt.data.image_url,
-                });
-            }
-            if (role === "org:student") {
-                await Student.findOneAndUpdate({ clerkID: evt.data.id }, {
-                    firstName: evt.data.first_name,
-                    lastName: evt.data.last_name,
-                    email: evt.data.email_addresses[0].email_address,
-                    photo: evt.data.image_url,
-                    department: evt.data.private_metadata?.department,
-                    semester: evt.data.private_metadata?.semester,
-                    rollNo: evt.data.private_metadata?.rollNo,
-                });
-            }
-        }
-    }
-    if (eventType === "user.deleted") {
-        await User.findOneAndDelete({ clerkID: evt.data.id });
-        await Faculty.findOneAndDelete({ clerkID: evt.data.id });
-        await Student.findOneAndDelete({ clerkID: evt.data.id });
-    }
-    if (eventType === "organization.created") {
-        await College.create({
-            admin: evt.data.private_metadata?._id, // admin mongoose ID
-            name: evt.data.name,
-            location: evt.data.private_metadata?.location,
-            logo: evt.data.image_url,
-        });
-    }
-    if (eventType === "organization.updated") {
-        await College.findOneAndUpdate({ clerkID: evt.data.id }, {
-            name: evt.data.name,
-            location: evt.data.private_metadata?.location,
-            logo: evt.data.image_url,
-        });
-    }
-    if (eventType === "organization.deleted") {
-        await College.findOneAndDelete({ clerkID: evt.data.id });
-    }
-    if (eventType === "organizationMembership.created") {
-        if (evt.data.role === "org:faculty") {
-
-        }
-        if (evt.data.role === "org:student") {
-
-        }
-    }
-    if (eventType === "organizationMembership.updated") {
-        if (evt.data.role === "org:faculty") {
-
-        }
-        if (evt.data.role === "org:student") {
-
-        }
-    }
-    if (eventType === "organizationMembership.deleted") {
-        if (evt.data.role === "org:faculty") {
-
-        }
-        if (evt.data.role === "org:student") {
-
-        }
     }
     return new Response("", { status: 200 });
 }

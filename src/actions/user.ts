d49d72bf7cfd642578945e7646b11f90e5eb16e3
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-07-04 16:02:44 +0530

    FEAT: Added Clerk Data Integration to MongoDB

---
+++ src/actions/user.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { connectDB } from "@/lib/database/connectDB";
+import User, { UserType } from "@/models/User.Model";
+
+export async function createUser(user: UserType) {
+    try {
+        await connectDB();
+        await User.create
+    }
+    catch (err: any) {
+        return { status: 500, message: err.message };
+    }
+}
